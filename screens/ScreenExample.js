eeb4b07283cc5139bed4cc12ee4588dd68439481
martamullor <marta.mullor.polo@gmail.com>
2020-10-13 21:21:58 +0200

    Standart template for react native

--- screens/ScreenExample.js
+++ screens/ScreenExample.js
@@ -1,14 +1,6 @@
 import * as React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import * as Localization from 'expo-localization';
 import i18n from 'i18n-js';
-
-i18n.translations = {
-  ca: { homeContent: 'Benvingut', screenExample: 'Fundació.cat català' },
-};
-i18n.locale = Localization.locale;
-i18n.locale = 'ca';
-i18n.fallbacks = true;
 
 export default function ScreenExample() {
   return (
