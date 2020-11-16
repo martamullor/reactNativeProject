7b33d108ce6b0d315f77e8130d168a4d1e8240ab
Marta Mullor <marta.mullor.polo@gmail.com>
2020-11-16 20:01:21 +0100

    feature: Migrated register screen to hooks

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { View, StyleSheet, StatusBar } from 'react-native';
 import i18n from '../i18';
 import { MyAppInput } from '../components/MyAppInput';
@@ -9,88 +9,65 @@ import firebase from '../services/firebase';
 import { heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import { GeneralStyles } from '../style/AppStyles';
 
-export default class RegisterScreen extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: '',
-      password: '',
-      errorMessage: '',
-      loading: false,
-    };
-  }
+const RegisterScreen = ({ navigation }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [setErrorMessage] = useState('');
 
-  onLoginSuccess() {
-    const { navigation } = this.props;
+  const onLoginSuccess = () => {
     navigation.navigate('ScreenExample');
-  }
-  onLoginFailure(errorMessage) {
-    this.setState({ error: errorMessage, loading: false });
-  }
+  };
 
-  onChangeEmail = (email) => this.setState({ email });
-
-  onChangePassword = (password) => this.setState({ password });
-
-  async signInWithEmail() {
+  const signInWithEmail = async () => {
     await firebase
       .auth()
-      .createUserWithEmailAndPassword(this.state.email, this.state.password)
-      .then(this.onLoginSuccess.bind(this))
+      .createUserWithEmailAndPassword(email, password)
+      .then(onLoginSuccess)
       .catch((error) => {
-        console.log(error);
         alert(error);
         let errorCode = error.code;
         let errorMessage = error.message;
         if (errorCode === 'auth/weak-password') {
-          this.onLoginFailure.bind(this)('Weak Password!');
+          setErrorMessage('Weak Password!');
         } else {
-          this.onLoginFailure.bind(this)(errorMessage);
+          setErrorMessage(errorMessage);
         }
       });
-  }
-
-  render() {
-    const { navigation } = this.props;
-    const { email, password } = this.state;
-    return (
-      <View style={GeneralStyles.contentAligned}>
-        <StatusBar barStyle="dark-content" />
-        <MyAppTitle style={styles.title}>{i18n.t('LOGIN_TITLE')}</MyAppTitle>
-        <MyAppInput
-          placeholder={i18n.t('LOGIN_PLACEHOLDER_EMAIL')}
-          value={email}
-          onChangeText={this.onChangeEmail}
-          label={i18n.t('LOGIN_LABEL_EMAIL')}
-          style={styles.input}
-        />
-        <MyAppInput
-          placeholder={i18n.t('LOGIN_PLACEHOLDER_PASSWORD')}
-          value={password}
-          onChangeText={this.onChangePassword}
-          label={i18n.t('LOGIN_LABEL_PASSWORD')}
-          secureTextEntry={true}
-          style={styles.input}
-        />
-        <MyAppButton
-          style={styles.button}
-          onPress={() => this.signInWithEmail()}
-        >
-          {i18n.t('REGISTER')}
-        </MyAppButton>
-        <MyAppButton
-          linkButton
-          style={styles.button}
-          onPress={() => {
-            navigation.navigate('LoginScreen');
-          }}
-        >
-          {i18n.t('REGISTER_BUTTON')}
-        </MyAppButton>
-      </View>
-    );
-  }
-}
+  };
+  return (
+    <View style={GeneralStyles.contentAligned}>
+      <StatusBar barStyle="dark-content" />
+      <MyAppTitle style={styles.title}>{i18n.t('LOGIN_TITLE')}</MyAppTitle>
+      <MyAppInput
+        placeholder={i18n.t('LOGIN_PLACEHOLDER_EMAIL')}
+        value={email}
+        onChangeText={setEmail}
+        label={i18n.t('LOGIN_LABEL_EMAIL')}
+        style={styles.input}
+      />
+      <MyAppInput
+        placeholder={i18n.t('LOGIN_PLACEHOLDER_PASSWORD')}
+        value={password}
+        onChangeText={setPassword}
+        label={i18n.t('LOGIN_LABEL_PASSWORD')}
+        secureTextEntry={true}
+        style={styles.input}
+      />
+      <MyAppButton style={styles.button} onPress={signInWithEmail}>
+        {i18n.t('REGISTER')}
+      </MyAppButton>
+      <MyAppButton
+        linkButton
+        style={styles.button}
+        onPress={() => {
+          navigation.navigate('LoginScreen');
+        }}
+      >
+        {i18n.t('REGISTER_BUTTON')}
+      </MyAppButton>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   title: {
@@ -103,3 +80,5 @@ const styles = StyleSheet.create({
     marginVertical: hp('3%'),
   },
 });
+
+export default RegisterScreen;
